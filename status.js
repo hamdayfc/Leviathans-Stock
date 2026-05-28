d7469f55bb9068b2f916f4b3467663b4cdc123de
hamdayfc <hamoodyrt46@gmail.com>
2026-05-29 01:22:03 +0300

    Update bot status activity name to 'BY VBC'

--- status.js
+++ status.js
@@ -1,7 +1,7 @@
 // دالة لتشغيل حالة الـ Playing للبوت
 function setBotStatus(client) {
   client.user.setPresence({
-    activities: [{ name: '/By vbc', type: 0 }], // نوع 0 يعني Playing (يلعب)
+    activities: [{ name: 'BY VBC', type: 0 }], // نوع 0 يعني Playing (يلعب)
     status: 'online', // الدائرة الخضراء
   });
   console.log("🎮 Bot Status (Playing /help) has been loaded successfully!");
