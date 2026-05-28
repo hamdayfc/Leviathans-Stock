a7d0c927d0cbbf8b59d34eeba1d40d70f84144ce
hamdayfc <hamoodyrt46@gmail.com>
2026-05-29 01:18:15 +0300

    Add bot status setting function

---
+++ status.js
@@ -0,0 +1,10 @@
+// دالة لتشغيل حالة الـ Playing للبوت
+function setBotStatus(client) {
+  client.user.setPresence({
+    activities: [{ name: '/By vbc', type: 0 }], // نوع 0 يعني Playing (يلعب)
+    status: 'online', // الدائرة الخضراء
+  });
+  console.log("🎮 Bot Status (Playing /help) has been loaded successfully!");
+}
+
+module.exports = { setBotStatus };
